607f9644301c13070b26296a9eecb05f0f8b8cd0
Rafee Memon <rafeememon@gmail.com>
2018-12-15 10:08:31 -0800

    Add react earthquakes demo

---
+++ src/demo/earthquakes/demo-react.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Table } from "../../react";
+import { ISort } from "../../types";
+import { COLUMNS } from "./columns";
+import { fetchEarthquakes } from "./data";
+import { IGeoJsonFeatureProperties } from "./types";
+
+import "./styles.css";
+
+interface IState {
+    rows: IGeoJsonFeatureProperties[];
+    selection: Set<string>;
+    sort: ISort<IGeoJsonFeatureProperties> | undefined;
+}
+
+class Demo extends React.PureComponent<{}, IState> {
+
+    public state: IState = {
+        rows: [],
+        selection: new Set(),
+        sort: undefined,
+    };
+
+    public componentDidMount() {
+        fetchEarthquakes().then((response) => {
+            const rows = response.features.map((feature) => feature.properties);
+            this.setState({ rows });
+        });
+    }
+
+    public render() {
+        return (
+            <>
+                <h1>All earthquakes in the past day</h1>
+                <Table
+                    keyField="code"
+                    rows={this.state.rows}
+                    columns={COLUMNS}
+                    selection={this.state.selection}
+                    selectionMode="multi"
+                    sort={this.state.sort}
+                    onSelect={this.handleSelect}
+                    onSort={this.handleSort}
+                />
+            </>
+        );
+    }
+
+    private handleSelect = (selection: Set<string>) => {
+        this.setState({ selection });
+    }
+
+    private handleSort = (sort: ISort<IGeoJsonFeatureProperties>) => {
+        this.setState({ sort });
+    }
+
+}
+
+const fixture = document.createElement("div");
+document.body.appendChild(fixture);
+ReactDOM.render(<Demo />, fixture);
